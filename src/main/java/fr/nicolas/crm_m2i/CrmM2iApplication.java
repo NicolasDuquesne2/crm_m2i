061c06500df3e090aa6b8f79b2dfb94c97aef18c
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2023-07-31 09:36:09 +0200

    Launch: set the new project

---
+++ src/main/java/fr/nicolas/crm_m2i/CrmM2iApplication.java
@@ -0,0 +1,13 @@
+package fr.nicolas.crm_m2i;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmM2iApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmM2iApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/nicolas/crm_m2i/CrmM2iApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.nicolas.crm_m2i;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmM2iApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
